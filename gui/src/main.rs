f6e9602ab45c6ab9bedc9a8f2d244bbdd535e5c9
Max T <maxtyuterev@gmail.com>
2024-10-03 22:00:24 -0400

    Update to newest iced and add file sending

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,11 +1,12 @@
 mod ws_client_subscription;
 
+use std::fs::File;
+use std::io::Read;
+
 use futures::channel::mpsc;
 
-use iced::executor::Default as DefaultExecutor;
-use iced::widget::{button, column, container, row, scrollable, text, text_input, Column};
-use iced::{Application, Command, Element, Length, Settings, Theme};
-use iced_aw::widgets::Modal;
+use iced::widget::{button, column, container, row, scrollable, stack, text, text_input, Column};
+use iced::{application, Application, Element, Length, Settings, Subscription, Task, Theme};
 use iced_aw::Card;
 use shared_types::messages::{self, ClientMessage};
 use ws_client_subscription::{start_client, ClientCommand};
@@ -18,6 +19,7 @@ enum AppUpdateMessage {
     UpdateAppView(AppView),
     UpdateChatInput(String),
     SendMessage(String),
+    AttemptSendFile,
     AttemptToConnect,
     MessageReceived(ClientMessage),
     SetError(String),
@@ -42,188 +44,209 @@ struct Messenger {
     chat_input: String,
 }
 
-impl Application for Messenger {
-    type Executor = DefaultExecutor;
+fn title(app: &Messenger) -> String {
+    String::from("Msger")
+}
 
-    type Message = AppUpdateMessage;
-
-    type Theme = Theme;
-
-    type Flags = ();
-
-    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
-        (Messenger::default(), Command::none())
-    }
-
-    fn title(&self) -> String {
-        String::from("Msger")
-    }
-
-    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
-        match message {
-            AppUpdateMessage::AttemptToConnect => {
-                if let Some(ref mut sender) = self.client_channel {
-                    let try_send = sender.try_send(ClientCommand::Connect(
-                        self.username.clone(),
-                        self.server_address.clone(),
-                    ));
-                    match try_send {
-                        Ok(_) => {}
-                        Err(err) => {
-                            self.error_message = Some(err.to_string());
-                        }
-                    }
+fn update(app: &mut Messenger, message: AppUpdateMessage) -> iced::Task<AppUpdateMessage> {
+    match message {
+        AppUpdateMessage::AttemptToConnect => {
+            if let Some(ref mut sender) = app.client_channel {
+                let try_send = sender.try_send(ClientCommand::Connect(
+                    app.username.clone(),
+                    app.server_address.clone(),
+                ));
+                match try_send {
+                    Ok(_) => {}
+                    Err(err) => app.error_message = Some(err.to_string()),
                 }
             }
-
-            AppUpdateMessage::SetError(error_message) => {
-                self.error_message = Some(error_message);
-            }
-            AppUpdateMessage::ResetError => {
-                self.error_message = None;
-            }
-            AppUpdateMessage::Disconnect => {
-                if let Some(ref mut sender) = self.client_channel {
-                    let try_send = sender.try_send(ClientCommand::Disconnect);
-                    match try_send {
-                        Ok(_) => {
-                            self.messages.clear();
-                        }
-                        Err(err) => {
-                            self.error_message = Some(err.to_string());
-                        }
-                    }
-                }
-            }
-            AppUpdateMessage::UpdateUsername(new_username) => {
-                self.username = new_username;
-            }
-            AppUpdateMessage::UpdateServerAddress(new_server_address) => {
-                self.server_address = new_server_address;
-            }
-            AppUpdateMessage::AppReady(sender) => {
-                self.client_channel = Some(sender);
-            }
-            AppUpdateMessage::UpdateAppView(new_view) => {
-                self.app_view = new_view;
-            }
-            AppUpdateMessage::MessageReceived(new_message) => {
-                self.messages.push(new_message);
-            }
-            AppUpdateMessage::UpdateChatInput(new_chat_input) => {
-                self.chat_input = new_chat_input;
-            }
-            AppUpdateMessage::SendMessage(message) => {
-                if let Some(ref mut sender) = self.client_channel {
-                    let try_send = sender.try_send(ClientCommand::SendMessage(message.clone()));
-                    match try_send {
-                        Ok(_) => {}
-                        Err(err) => {
-                            self.error_message = Some(err.to_string());
-                        }
-                    }
-                }
-            }
-        };
-        Command::none()
-    }
-
-    fn view(&self) -> iced::Element<'_, Self::Message, Self::Theme, iced::Renderer> {
-        let ui: Element<AppUpdateMessage> = match &self.client_channel {
-            Some(_) => match self.app_view {
-                AppView::Login => {
-                    let name_input = text_input("username", &self.username)
-                        .on_input(AppUpdateMessage::UpdateUsername)
-                        .width(300)
-                        .padding(5);
-                    let server_address_input = text_input("wss://...", &self.server_address)
-                        .on_input(AppUpdateMessage::UpdateServerAddress)
-                        .width(300)
-                        .padding(5);
-
-                    let submit_message =
-                        if !self.username.is_empty() && !self.server_address.is_empty() {
-                            Some(AppUpdateMessage::AttemptToConnect)
-                        } else {
-                            None
-                        };
-                    let submit_button = button(text("Connect"))
-                        .on_press_maybe(submit_message)
-                        .padding(5);
-
-                    column!(name_input, server_address_input, submit_button)
-                        .align_items(iced::Alignment::Center)
-                        .spacing(10)
-                        .into()
-                }
-                AppView::Chat => {
-                    let messages = self.messages.iter().fold(
-                        Column::<AppUpdateMessage>::new()
-                            .align_items(iced::Alignment::Start)
-                            .spacing(2),
-                        |col, message| {
-                            let message_author = text(format!("{}:", message.author));
-                            let message_contents = match &message.contents {
-                                messages::MessageContents::Text(txt) => text(txt),
-                                messages::MessageContents::File(_) => text("This is a file wow"),
-                            };
-                            let message_row = row!(message_author, message_contents)
-                                .spacing(2)
-                                .align_items(iced::Alignment::End);
-                            col.push(message_row)
-                        },
-                    );
-                    let chat = scrollable(messages)
-                        .height(Length::FillPortion(9))
-                        .width(Length::Fill);
-
-                    let chat_input = text_input("Type your message...", &self.chat_input)
-                        .on_input(AppUpdateMessage::UpdateChatInput)
-                        .on_submit(AppUpdateMessage::SendMessage(self.chat_input.clone()))
-                        .padding(5);
-
-                    let chat_submit_button = button(text("Send"))
-                        .on_press(AppUpdateMessage::SendMessage(self.chat_input.clone()))
-                        .padding(5);
-                    let chat_disconnect_button = button(text("Disconnect"))
-                        .padding(5)
-                        .on_press(AppUpdateMessage::Disconnect);
-                    let controls = row!(chat_input, chat_submit_button, chat_disconnect_button)
-                        .height(Length::FillPortion(1));
-
-                    column!(chat, controls).into()
-                }
-            },
-            None => text("Loading...").into(),
-        };
-
-        if let Some(err) = &self.error_message {
-            let under_modal = container(ui)
-                .center_x()
-                .center_y()
-                .height(Length::Fill)
-                .width(Length::Fill);
-            let overlay = Card::new(text("An error has occured"), text(err));
-
-            let modal = Modal::new(under_modal, Some(overlay))
-                .backdrop(AppUpdateMessage::ResetError)
-                .on_esc(AppUpdateMessage::ResetError);
-            modal.into()
-        } else {
-            container(ui)
-                .center_x()
-                .center_y()
-                .height(Length::Fill)
-                .width(Length::Fill)
-                .into()
         }
-    }
 
-    fn subscription(&self) -> iced::Subscription<Self::Message> {
-        start_client()
+        AppUpdateMessage::SetError(error_message) => {
+            app.error_message = Some(error_message);
+        }
+        AppUpdateMessage::ResetError => {
+            app.error_message = None;
+        }
+        AppUpdateMessage::Disconnect => {
+            if let Some(ref mut sender) = app.client_channel {
+                let try_send = sender.try_send(ClientCommand::Disconnect);
+                match try_send {
+                    Ok(_) => {
+                        app.messages.clear();
+                    }
+                    Err(err) => {
+                        app.error_message = Some(err.to_string());
+                    }
+                }
+            }
+        }
+        AppUpdateMessage::UpdateUsername(new_username) => {
+            app.username = new_username;
+        }
+        AppUpdateMessage::UpdateServerAddress(new_server_address) => {
+            app.server_address = new_server_address;
+        }
+        AppUpdateMessage::AppReady(sender) => {
+            app.client_channel = Some(sender);
+        }
+        AppUpdateMessage::UpdateAppView(new_view) => {
+            app.app_view = new_view;
+        }
+        AppUpdateMessage::MessageReceived(new_message) => {
+            app.messages.push(new_message);
+        }
+        AppUpdateMessage::UpdateChatInput(new_chat_input) => {
+            app.chat_input = new_chat_input;
+        }
+        AppUpdateMessage::SendMessage(message) => {
+            if let Some(ref mut sender) = app.client_channel {
+                let try_send = sender.try_send(ClientCommand::SendMessage(message.clone()));
+                match try_send {
+                    Ok(_) => {}
+                    Err(err) => {
+                        app.error_message = Some(err.to_string());
+                    }
+                }
+            }
+        }
+        AppUpdateMessage::AttemptSendFile => {
+            let maybe_file = rfd::FileDialog::new()
+                .pick_file()
+                .and_then(|path_buf| File::open(path_buf).ok())
+                .and_then(|mut file| {
+                    let mut buf = vec![];
+                    file.read_to_end(&mut buf).map(|_| buf).ok()
+                });
+
+            if let Some(ref mut sender) = app.client_channel {
+                if let Some(buf) = maybe_file {
+                    let try_send = sender.try_send(ClientCommand::SendFile(buf));
+                    match try_send {
+                        Ok(_) => {}
+                        Err(err) => {
+                            app.error_message = Some(err.to_string());
+                        }
+                    }
+                }
+            }
+        }
+    };
+    Task::none()
+}
+
+fn view(app: &Messenger) -> iced::Element<AppUpdateMessage> {
+    let ui: Element<AppUpdateMessage> = match &app.client_channel {
+        Some(_) => match app.app_view {
+            AppView::Login => {
+                let name_input = text_input("username", &app.username)
+                    .on_input(AppUpdateMessage::UpdateUsername)
+                    .width(300)
+                    .padding(5);
+                let server_address_input = text_input("wss://...", &app.server_address)
+                    .on_input(AppUpdateMessage::UpdateServerAddress)
+                    .width(300)
+                    .padding(5);
+
+                let submit_message = if !app.username.is_empty() && !app.server_address.is_empty() {
+                    Some(AppUpdateMessage::AttemptToConnect)
+                } else {
+                    None
+                };
+                let submit_button = button(text("Connect"))
+                    .on_press_maybe(submit_message)
+                    .padding(5);
+
+                column!(name_input, server_address_input, submit_button)
+                    .align_x(iced::Alignment::Center)
+                    .spacing(10)
+                    .into()
+            }
+            AppView::Chat => {
+                let messages = app.messages.iter().fold(
+                    Column::<AppUpdateMessage>::new()
+                        .align_x(iced::Alignment::Start)
+                        .spacing(2),
+                    |col, message| {
+                        let message_author = text(format!("{}:", message.author));
+                        let message_contents = match &message.contents {
+                            messages::MessageContents::Text(txt) => text(txt),
+                            // TODO: Add functionality to actually download file
+                            messages::MessageContents::File(_) => text("This is a file wow"),
+                        };
+                        let message_row = row!(message_author, message_contents)
+                            .spacing(2)
+                            .align_y(iced::Alignment::End);
+                        col.push(message_row)
+                    },
+                );
+                let chat = scrollable(messages)
+                    .height(Length::FillPortion(9))
+                    .width(Length::Fill);
+
+                let chat_input = text_input("Type your message...", &app.chat_input)
+                    .on_input(AppUpdateMessage::UpdateChatInput)
+                    .on_submit(AppUpdateMessage::SendMessage(app.chat_input.clone()))
+                    .padding(5);
+
+                let chat_open_file_button = button(text("Open"))
+                    .on_press(AppUpdateMessage::AttemptSendFile)
+                    .padding(5);
+
+                let chat_submit_button = button(text("Send"))
+                    .on_press(AppUpdateMessage::SendMessage(app.chat_input.clone()))
+                    .padding(5);
+                let chat_disconnect_button = button(text("Disconnect"))
+                    .padding(5)
+                    .on_press(AppUpdateMessage::Disconnect);
+                let controls = row!(
+                    chat_input,
+                    chat_submit_button,
+                    chat_open_file_button,
+                    chat_disconnect_button
+                )
+                .height(Length::FillPortion(1));
+
+                column!(chat, controls).into()
+            }
+        },
+        None => text("Loading...").into(),
+    };
+
+    if let Some(err) = &app.error_message {
+        let under_modal = container(ui)
+            .center_x(Length::Fill)
+            .center_y(Length::Fill)
+            .height(Length::Fill)
+            .width(Length::Fill);
+
+        let overlay = Card::new(
+            text("An error has occured"),
+            column![
+                text(err),
+                button("Close").on_press(AppUpdateMessage::ResetError)
+            ],
+        );
+
+        let modal = stack![under_modal, overlay];
+
+        modal.into()
+    } else {
+        container(ui)
+            .center_x(Length::Fill)
+            .center_y(Length::Fill)
+            .height(Length::Fill)
+            .width(Length::Fill)
+            .into()
     }
 }
 
+fn subscription(_app: &Messenger) -> iced::Subscription<AppUpdateMessage> {
+    Subscription::run(start_client)
+}
+
 fn main() -> Result<(), iced::Error> {
-    Messenger::run(Settings::default())
+    let app = application(title, update, view).subscription(subscription);
+    app.run()
 }
--- gui/src/ws_client_subscription.rs
+++ gui/src/ws_client_subscription.rs
@@ -1,65 +1,67 @@
-use client::client::{connect, Client, WSMessage};
-use iced::subscription::channel;
+use client::client::{connect, Client};
+use futures::Stream;
+use iced::stream::channel;
 use iced::{futures, Subscription};
 
 use futures::channel::mpsc::{self, Receiver};
 use futures::sink::SinkExt;
-use futures::stream::{SplitSink, SplitStream, StreamExt};
+use futures::stream::StreamExt;
 use shared_types::messages::ClientMessage;
 
 use crate::AppUpdateMessage;
 
-pub fn start_client() -> Subscription<AppUpdateMessage> {
-    struct ClientBackgroundTask; // Type to keep track of unique task
-    let mut worker_state = WorkerState::Setup;
-    let mut client_state = ConnectionState::Idle;
-    channel(
-        std::any::TypeId::of::<ClientBackgroundTask>(),
-        100,
-        |mut output| async move {
-            loop {
-                match worker_state {
-                    WorkerState::Setup => {
-                        // Create channel
-                        let (sender, receiver) = mpsc::channel(100);
+enum WorkerState {
+    Setup,
+    Ready(Receiver<ClientCommand>),
+}
 
-                        let _ = output.send(AppUpdateMessage::AppReady(sender)).await;
-                        worker_state = WorkerState::Ready(receiver);
-                    }
-                    WorkerState::Ready(ref mut receiver) => match client_state {
-                        ConnectionState::Idle => {
-                            let input = receiver.select_next_some().await;
-                            match input {
-                                ClientCommand::Connect(username, server_address) => {
-                                    let try_client = connect(&username, &server_address).await;
-                                    match try_client {
-                                        Ok(client) => {
-                                            let _ = output
-                                                .send(AppUpdateMessage::UpdateAppView(
-                                                    crate::AppView::Chat,
-                                                ))
-                                                .await;
+enum ConnectionState {
+    Idle,
+    Connected(Client),
+}
 
-                                            client_state = ConnectionState::Connected(client);
-                                        }
-                                        Err(err) => {
-                                            let _ = output
-                                                .send(AppUpdateMessage::SetError(err.to_string()))
-                                                .await;
+pub enum ClientCommand {
+    Connect(String, String),
+    SendMessage(String),
+    SendFile(Vec<u8>),
+    Disconnect,
+}
 
-                                            client_state = ConnectionState::Idle;
-                                        }
-                                    }
-                                }
-                                _ => {}
-                            }
-                        }
-                        ConnectionState::Connected(ref mut client) => {
-                            futures::select! {
-                            received = client.select_next_some() => {
-                                match received {
-                                    Ok(message) => {
-                                       let _ = output.send(AppUpdateMessage::MessageReceived(message)).await;
+pub fn start_client() -> impl Stream<Item = AppUpdateMessage> {
+    // Set up a background worker to keep track of the WS connection in the Client
+    let mut worker_state = WorkerState::Setup; // Unique the the workier
+    let mut client_state = ConnectionState::Idle; // Unique to the client
+    channel(100, |mut output| async move {
+        loop {
+            match worker_state {
+                WorkerState::Setup => {
+                    //Basically we have no way to communicate with the frontend
+
+                    let (sender, receiver) = mpsc::channel(100);
+
+                    let _ = output.send(AppUpdateMessage::AppReady(sender)).await;
+
+                    // Now we have something
+                    worker_state = WorkerState::Ready(receiver);
+                }
+                WorkerState::Ready(ref mut receiver) => match client_state {
+                    ConnectionState::Idle => {
+                        let input = receiver.select_next_some().await;
+
+                        match input {
+                            ClientCommand::Connect(username, server_address) => {
+                                let try_client = connect(&username, &server_address).await;
+
+                                //Try to connect and abort if not possible
+                                match try_client {
+                                    Ok(client) => {
+                                        let _ = output
+                                            .send(AppUpdateMessage::UpdateAppView(
+                                                crate::AppView::Chat,
+                                            ))
+                                            .await;
+
+                                        client_state = ConnectionState::Connected(client);
                                     }
                                     Err(err) => {
                                         let _ = output
@@ -70,10 +72,33 @@ pub fn start_client() -> Subscription<AppUpdateMessage> {
                                     }
                                 }
                             }
+                            _ => {}
+                        }
+                    }
+                    ConnectionState::Connected(ref mut client) => {
+                        // Can think of this as either getting a command from frontent or from server
+                        futures::select! {
 
-                            command = receiver.select_next_some() => {
-                                match command {
-                                    ClientCommand::SendMessage(message) => {
+                            // The server wants us to do something
+                            received = client.select_next_some() => {
+                                match received {
+                                    Ok(message) => {
+                                        let _ = output.send(AppUpdateMessage::MessageReceived(message)).await;
+                                    }
+                                    Err(err) => {
+                                        let _ = output
+                                        .send(AppUpdateMessage::SetError(err.to_string()))
+                                        .await;
+
+                                    client_state = ConnectionState::Idle;
+                                }
+                            }
+                        }
+
+                        // The client wants us to do something
+                        command = receiver.select_next_some() => {
+                            match command {
+                                ClientCommand::SendMessage(message) => {
                                         match client.send_message(&message).await {
                                             Ok(_) => {
                                                 let _ = output
@@ -91,8 +116,6 @@ pub fn start_client() -> Subscription<AppUpdateMessage> {
                                                 client_state = ConnectionState::Idle;
                                             },
                                         }
-
-
                                     },
                                     ClientCommand::SendFile(file) => {
                                         match client.send_file(file.clone()).await {
@@ -125,27 +148,9 @@ pub fn start_client() -> Subscription<AppUpdateMessage> {
 
                                 }
                             }
-                        }
-                    },
-                }
+                    }
+                },
             }
-        },
-    )
-}
-
-enum WorkerState {
-    Setup,
-    Ready(Receiver<ClientCommand>),
-}
-
-enum ConnectionState {
-    Idle,
-    Connected(Client),
-}
-
-pub enum ClientCommand {
-    Connect(String, String),
-    SendMessage(String),
-    SendFile(Vec<u8>),
-    Disconnect,
+        }
+    })
 }
